84dfb6548ee374c4c8c47e15282e154eccdb8e5c
Aika Okemoto <aika2004oke@gmail.com>
2020-06-13 19:02:40 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by 桶本あいか on 2020/06/13.
+//  Copyright © 2020 Okemoto.Okke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
